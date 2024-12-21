3929064c00fc8dda700d5fb0d0f5642521b9df02
LukeZHar <15649@coderacademy.edu.au>
2024-12-21 10:01:55 +1000

    feat: Added one more test case for not requiring auth header for the root route GET /

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
-// Here we configure the application server
+// Here, we configure the application server
 
 // Reason for separation:
 // Don't often need to start the server
-// Can import the app without having to start the server
+// Can import the app without worrying about starting the server
 
-const express = require('express');
+// Import express
+const express = require("express");
 
 // Make an instance of the express server
 const app = express();
 
-// Start defininf routes: instance.verb(url, middleware/callback)
-// GET localhost:3000/
+// Start defining routes: instance.verb(url, middleware/callback)
+// GET localhost:3300/
 app.get("/", (req, res) => {
     res.json({
         message: "Hello World!"
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,17 +1,26 @@
-// importing the code that we want to test
+// Importing the code that we want to test
 
-const {app} = require('../src/server.js');
+const { app } = require("../src/server.js");
 // Importing a testing helper function from supertest
-const request = require('supertest');
+const request = require("supertest");
 
 // Syntax: describe(desc, callback test function)
-describe("Root route"), () => {
+describe("Root route", () => {
     // Syntax: test/it (ack msg, callback)
     test("Server returns a 'Hello World!' message", async () => {
         // Make a request to the '/' route
         // wait for response and store it
         const response = await request(app).get("/");
+
         // check the body of the response to see if it matches the 'Hello World!' string
         expect(response.body.message).toBe("Hello World!");
+        expect(response.statusCode).toBe(200);
     });
-};
+
+    test("Server returns a reponse without Auth header", async () => {
+        const response = await request(app).get("/");
+        console.log(response);
+
+        expect(response.headers["Authorisation"]).toBeFalsy();
+    });
+});
